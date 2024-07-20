74b36133ffac7bffd99ea9f28a1b36837cbbdc76
Cole Neild <cole.neild@gmail.com>
2024-07-20 16:04:31 -0400

    summed value

---
+++ sum_05/sumAll.js
@@ -0,0 +1,10 @@
+sumAll(1, 4)
+const sumAll = function(index) {
+    const SummedValue = 0;
+    for (let index = 0; index < array.length; index++) {
+      SummedValue += index;
+    }
+};
+
+// Do not edit below this line
+module.exports = sumAll;
